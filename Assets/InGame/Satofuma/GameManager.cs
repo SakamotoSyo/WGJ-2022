2a61e1162cd09e09a63bd0e5666c3677b8163744
sato_310 <fuuuma.s@gmail.com>
2022-12-22 11:08:57 +0900

    シーンの追加 Gamemanager,LoadScenesの追加

---
+++ Assets/InGame/Satofuma/GameManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static int score;
+    public static void Addscore(int scorepoint)
+    {
+        score += scorepoint;
+    }
+}
