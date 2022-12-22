e934dd807d197070afa5101e02d32578f64c4a8e
sato_310 <fuuuma.s@gmail.com>
2022-12-22 14:46:06 +0900

    ランキングの追加

--- Assets/InGame/Satofuma/SceneTranstion.cs
+++ Assets/InGame/Satofuma/SceneTranstion.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-public class LoadScenes : MonoBehaviour
+public class SceneTranstion : MonoBehaviour
 {
     public void Loadscene(int index)//ƒV[ƒ““Ç‚İ‚İ
     {
