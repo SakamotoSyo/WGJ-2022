30b6fa9dc14d63f7c683bcd83b884ad0d8ee968f
sakamotosyo <syo10150000@gmail.com>
2022-12-22 17:02:39 +0900

    [Fix]バグの修正

--- Assets/InGame/Satofuma/SceneTranstion.cs
+++ Assets/InGame/Satofuma/SceneTranstion.cs
@@ -8,9 +8,15 @@ public class SceneTranstion : MonoBehaviour
     {
         SceneManager.LoadScene(index);
     }
+    public static void LoadScene(string l)
+    {
+        SceneManager.LoadScene(l);
+    }
+
     public void MainScene()
     {
         GameManager.ResetScore();
+        AudioManager.Instance.Reset();
         SceneManager.LoadScene("MainScene");
     }
 }
